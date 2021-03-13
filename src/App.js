ddc3959f5b064ae129c9e5b13616dc655af50a02
harshal patil <harshpatilheros@gmail.com>
2021-03-13 19:21:52 +0530

    state method demo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="harsh">
+     {/*<Greet name="harsh">
       <p>Welcome</p></Greet>
       <Greet name="ram"/>
       <Greet name="harry"/>
       <Welcome name="potter"/>
-      <Welcome name="wisely"/>
+  <Welcome name="wisely"/> */}
+      <Message/>
 
     </div>
   );
